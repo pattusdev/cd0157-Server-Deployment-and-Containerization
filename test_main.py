f3386a83c3d9ed8f8af3d51a0c67346800c54c6e
Rwego Patrick <rwego.patrick.50@gmail.com>
2022-10-04 16:59:21 +0200

    FInalTest

--- test_main.py
+++ test_main.py
@@ -26,9 +26,8 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
-    assert False
 
-
+    #assert False
 '''
 Assert False
 '''
